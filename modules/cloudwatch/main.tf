b9cb37b67fc44a8530452e495102044c7657cf6f
Sheryll Agpalo <sagpalo@salesforce.com>
2019-01-24 10:43:46 -0800

    change schedule from daily to hourly

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_event_rule" "gitrepo" {
   name                = "${var.regional_prefix}-cw-schedule"
   description         = "Cloud watch Scheduler to ensure the branch protection rules are intact for git repository"
-  schedule_expression = "rate(1 day)"
+  schedule_expression = "rate(1 hour)"
 }
 
 resource "aws_cloudwatch_event_target" "gitrepo_cw_lambda" {
