43623890f111ef47352449bf68e3ef69f3257723
Sreekanth Tadakamalla <stadakamalla@salesforce.com>
2019-02-01 12:39:10 +0530

    added status checks

--- envs/ssdev-or/ssdev-or-vars.tf
+++ envs/ssdev-or/ssdev-or-vars.tf
@@ -1,14 +1,6 @@
 module "ssdev" {
   source = "../../modules/main"
 
-  #------------------------------------------------------
-  # Global Tags
-  #------------------------------------------------------
-  technology = "Git Repository"
-
-  technology_code = "grepo"
-  created_by      = "aws-tf-proj-gitrepo"
-
   # lambda variables
   secret_name = "aws-tf-proj-plan_alert-github_token"
   group_name  = "ssdev-fundn-group-infradev"
--- envs/ssprod-or/ssprod-or-vars.tf
+++ envs/ssprod-or/ssprod-or-vars.tf
@@ -1,14 +1,6 @@
 module "ssprod" {
   source = "../../modules/main"
 
-  #------------------------------------------------------
-  # Global Tags
-  #------------------------------------------------------
-  technology = "Git Repository"
-
-  technology_code = "grepo"
-  created_by      = "aws-tf-proj-gitrepo"
-
   # lambda variables
   secret_name = "aws-tf-proj-plan_alert-github_token"
   group_name  = "ssprod-fundn-group-infradev"
