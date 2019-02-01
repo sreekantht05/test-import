5fa7de25a7065ed71f3727446ee8c3a821127f8c
Sreekanth Tadakamalla <stadakamalla@salesforce.com>
2019-02-01 18:03:41 +0530

    multi region names added

--- envs/ssdev-or/ssdev-or-outputs.tf
+++ envs/ssdev-or/ssdev-or-outputs.tf
@@ -1,11 +1,11 @@
 output "service-role-arn" {
-  value = "${module.ssdev.service-role-arn}"
+  value = "${module.ssdev-or.service-role-arn}"
 }
 
 output "lambda-arn" {
-  value = "${module.ssdev.lambda-arn}"
+  value = "${module.ssdev-or.lambda-arn}"
 }
 
 output "cloudwatch-arn" {
-  value = "${module.ssdev.cloudwatch-arn}"
+  value = "${module.ssdev-or.cloudwatch-arn}"
 }
