838de0580439dce1e1ff700b9da235eec110117f
Sreekanth Tadakamalla <stadakamalla@salesforce.com>
2019-02-11 18:25:09 +0530

    added additional check for terraform pipeline statuscheck

--- envs/ssprod-or/ssprod-or-vars.tf
+++ envs/ssprod-or/ssprod-or-vars.tf
@@ -1,8 +1,8 @@
-# module "ssprod-or" {
-#   source = "../../modules/main"
+module "ssprod-or" {
+  source = "../../modules/main"
 
-#   # lambda variables
-#   secret_name = "aws-tf-proj-plan_alert-github_token"
-#   group_name  = "ssprod-fundn-group-infradev"
-#   role_name   = "ssprod-fundn-role-infradev"
-# }
+  # lambda variables
+  secret_name = "aws-tf-proj-plan_alert-github_token"
+  group_name  = "ssprod-fundn-group-infradev"
+  role_name   = "ssprod-fundn-role-infradev"
+}
